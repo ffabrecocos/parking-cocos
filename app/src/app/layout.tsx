2e72c9df2eb97c4550a87ee77fa1e06f2d6691c5
Federico Fabre <ffabrecocos@users.noreply.github.com>
2026-06-23 12:40:08 -0300

    Updating icons

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -6,7 +6,11 @@ export const metadata: Metadata = {
   description: "Reservá tu cochera en la oficina",
   manifest: "/manifest.webmanifest",
   icons: {
-    apple: "/cocos-logo.png",
+    icon: [
+      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: "/icons/apple-touch-icon.png",
   },
   appleWebApp: {
     capable: true,
